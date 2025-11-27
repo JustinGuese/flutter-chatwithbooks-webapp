90d72cbca06a1fa7f56fa63a9e4b9673118f6968
Justin Guese <guese.justin@gmail.com>
2025-11-27 11:33:27 +0100

    Update webapp

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"b5990e5ccc5e325fd24f0746e7d6689bbebc7c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2483566313"
+    serviceWorkerVersion: "982861347"
   }
 });
